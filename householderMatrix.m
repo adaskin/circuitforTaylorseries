35c2b6f21233c833bcb2b58893a6dd4430e3325d
adaskin <emmar.daskin@gmail.com>
2018-01-25 15:46:33 +0300

    Update householderMatrix.m

--- householderMatrix.m
+++ householderMatrix.m
@@ -1,18 +1,18 @@
 function H = householderMatrix (x)
 % forms a Householder matrix for the vector u, 
 %the first row column of the matrix is u.
- % u is a column vector;
+% u is a column vector;
 
-	 m = length(x);
-	% u=u/max(abs(u));
-	 if x(1) == 0
-	    sx = 1;
-	 else
-	   sx = sign(x(1));
-	 end
-	 u = x + sx*eye(m,1);
-	 u = u/norm(u);
-	 I = eye(m);
-	 H = I-(2*u)*u';
-	 H = H./(-sx);
+m = length(x);
+
+if x(1) == 0
+    sx = 1;
+else
+    sx = sign(x(1));
+end
+u = x + sx*eye(m,1);
+u = u/norm(u);
+I = eye(m);
+H = I - (2*u)*u';
+H = H./(-sx);
 end
